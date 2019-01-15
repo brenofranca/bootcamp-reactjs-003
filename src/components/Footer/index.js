86b9e67a9c6320fce364afa9e3eca3ec4481f971
Breno França <franciscobreno.si@gmail.com>
2019-01-14 23:10:20 -0300

    footer - adicionado o component de footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { Container } from './styles';
+
+const Footer = ({ count }) => (
+  <Container>
+    <p>
+      Você possui
+      {' '}
+      <strong>{count}</strong>
+      {' '}
+repositórios favoritos
+    </p>
+  </Container>
+);
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+  count: state.repositories.length,
+});
+
+export default connect(mapStateToProps)(Footer);
