cde0b63d9edf3e3209a6133461dc286aa249ae5f
Breno França <franciscobreno.si@gmail.com>
2019-01-14 22:38:47 -0300

    reactotron - adicionado e configurado o reacton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
 
+import './config/reactotron';
+
 import store from './store';
 
 import Main from './pages/Main';
